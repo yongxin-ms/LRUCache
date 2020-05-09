5bc5caa3d9079cdb5cc249ce0ded5fc1c98e54a5
Will <will@i007.cc>
2020-05-10 03:01:08 +0800

    功能完善

--- examples/build.bat
+++ examples/build.bat
@@ -1,9 +1,6 @@
 @set trunk_dir=%cd%
 pushd %trunk_dir%
 
-cd proto 
-call protogen.bat
-
 md %trunk_dir%\build
 cd %trunk_dir%\build
 
--- include/lru_cache.h
+++ include/lru_cache.h
@@ -1,1 +1,55 @@
-﻿#pragma once
+#pragma once
+#include <list>
+#include <map>
+
+namespace lru_cache {
+
+template <typename Key, typename Value>
+class LRUCache {
+public:
+	LRUCache(int capacity)
+		: capacity_(capacity) {}
+
+	Value get(Key key) {
+		auto it = values_.find(key);
+		if (it == values_.end())
+			return -1;
+		auto& it_in_list = it->second.second;
+		latest_use_.erase(it_in_list);
+		latest_use_.push_back(key);
+		it_in_list = latest_use_.end();
+		it_in_list--;
+		return it->second.first;
+	}
+
+	void put(int key, int value) {
+		auto it = values_.find(key);
+		if (it == values_.end()) {
+			//没在里面
+			if (latest_use_.size() >= capacity_) {
+				auto v = latest_use_.front();
+				values_.erase(v);
+				latest_use_.pop_front();
+			}
+
+			latest_use_.push_back(key);
+			auto it_in_list = latest_use_.end();
+			it_in_list--;
+			values_[key] = std::make_pair(value, it_in_list);
+		} else {
+			it->second.first = value;
+			auto& it_in_list = it->second.second;
+			latest_use_.erase(it_in_list);
+			latest_use_.push_back(key);
+			it_in_list = latest_use_.end();
+			it_in_list--;
+		}
+	}
+
+private:
+	std::map<Key, std::pair<Key, std::list<Key>::const_iterator>> values_;
+	std::list<Key> latest_use_;
+	int capacity_;
+};
+
+} // namespace lru_cache
